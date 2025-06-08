5338251c0e3e700601685ed00d2dd06a4540c6ed
saperate <61527642+saperate@users.noreply.github.com>
2025-06-08 13:36:52 -0400

    Bug fixing (1.1.1)

--- src/main/java/dev/saperate/items/DragonTestosteroneItem.java
+++ src/main/java/dev/saperate/items/DragonTestosteroneItem.java
@@ -2,6 +2,7 @@ package dev.saperate.items;
 
 import com.iafenvoy.iceandfire.entity.EntityDragonBase;
 import dev.saperate.effects.DragonHRTEffects;
+import dev.saperate.utils.SapsUtils;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
@@ -17,7 +18,10 @@ public class DragonTestosteroneItem extends Item {
 
     @Override
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
-        if(entity instanceof EntityDragonBase dragon  && !dragon.isMale()){
+        if (entity instanceof EntityDragonBase dragon && dragon.isOwner(user) && !dragon.isMale() &&
+                !SapsUtils.safeHasStatusEffect(DragonHRTEffects.ESTROGEN_EFFECT, entity) &&
+                !SapsUtils.safeHasStatusEffect(DragonHRTEffects.TESTOSTERONE_EFFECT, entity)
+        ) {
             dragon.addStatusEffect(new StatusEffectInstance(
                     DragonHRTEffects.TESTOSTERONE_EFFECT,
                     48000,
