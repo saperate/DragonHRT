414b180c3b564ec6833399a297a1ba90eb73f444
saperate <61527642+saperate@users.noreply.github.com>
2025-06-08 13:25:57 -0400

    forgot the recipes

--- src/main/java/dev/saperate/items/DragonHRTITems.java
+++ src/main/java/dev/saperate/items/DragonHRTITems.java
@@ -2,12 +2,17 @@ package dev.saperate.items;
 
 import dev.saperate.DragonHRT;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class DragonHRTITems {
+    
     public static final DragonEstrogenItem DRAGON_ESTROGEN_ITEM = (DragonEstrogenItem)
             registerItem("dragon_estrogen",
                     new DragonEstrogenItem(new FabricItemSettings().maxCount(16)));
@@ -20,7 +25,18 @@ public class DragonHRTITems {
             registerItem("hormone_blocker",
                     new HormoneBlockerItem(new FabricItemSettings().maxCount(16)));
 
-    public static void register() {}
+    public static  final ItemGroup DRAGONHRT_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(DRAGON_ESTROGEN_ITEM))
+            .displayName(Text.of("DragonHRT"))
+            .entries((context,entries) -> {
+                entries.add(DRAGON_ESTROGEN_ITEM);
+                entries.add(DRAGON_TESTOSTERONE_ITEM);
+                entries.add(HORMONE_BLOCKER_ITEM);
+            }).build();
+    
+    public static void register() {
+        Registry.register(Registries.ITEM_GROUP, Identifier.of(DragonHRT.MODID, "group"), DRAGONHRT_GROUP);
+    }
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(DragonHRT.MODID, name), item);
